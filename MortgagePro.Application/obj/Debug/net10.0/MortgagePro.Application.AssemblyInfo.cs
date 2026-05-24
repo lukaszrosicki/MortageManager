9c1a6f5e1466cd705feb2e3feaa41677755df2fd
Lukasz <rosicki.lukasz@gmail.com>
2026-05-24 11:35:25 +0200

    Dodanie bazy danych i poprawki w widoku

--- MortgagePro.Application/obj/Debug/net10.0/MortgagePro.Application.AssemblyInfo.cs
+++ MortgagePro.Application/obj/Debug/net10.0/MortgagePro.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MortgagePro.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+940916d8a7b3f42317037fd9e83192c5eda5891e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57027f9ec56ca3db176619e02e46610dd0ff6112")]
 [assembly: System.Reflection.AssemblyProductAttribute("MortgagePro.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MortgagePro.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
